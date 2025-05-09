cc7a35895e51091b743e362402de453d90b24c0f
Jan Ryan Ancheta <jan.ryan.ancheta@proton.me>
2025-05-09 17:29:41 +0800

    Addition of priority scheduling algorithm

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-from scheduler import Process, RoundRobinScheduler
+from scheduler import Process, RoundRobinScheduler, PriorityScheduler
 import pandas as pd
 
 def main():
     
     # sample process data (process_id, arrival_time, burst_time)
     process_list = [
-        (1, 0, 5),   # Process 1: arrives at time 0, needs 5 units of burst time
-        (2, 1, 3),   # Process 2: arrives at time 1, needs 3 units of burst time
-        (3, 2, 8),   # Process 3: arrives at time 2, needs 8 units of burst time
-        (4, 3, 6),   # Process 4: arrives at time 3, needs 6 units of burst time
-        (5, 4, 4),   # Process 5: arrives at time 4, needs 4 units of burst time
-        (6, 5, 7),   # Process 6: arrives at time 5, needs 7 units of burst time
-        (7, 6, 2),   # Process 7: arrives at time 6, needs 2 units of burst time
-        (8, 7, 9),   # Process 8: arrives at time 7, needs 9 units of burst time
-        (9, 8, 5),   # Process 9: arrives at time 8, needs 5 units of burst time
-        (10, 9, 4),  # Process 10: arrives at time 9, needs 4 units of burst time
+        (1, 0, 5, 2),   # Process 1: arrives at time 0, needs 5 units of burst time
+        (2, 1, 3, 1),   # Process 2: arrives at time 1, needs 3 units of burst time
+        (3, 2, 8, 0),   # Process 3: arrives at time 2, needs 8 units of burst time
+        (4, 3, 6, 10),   # Process 4: arrives at time 3, needs 6 units of burst time
+        (5, 4, 4, 5),   # Process 5: arrives at time 4, needs 4 units of burst time
     ]
     
     # Time quanta for Round Robin scheduling (e.g., 4 units)
     time_quanta = 4
     
     # Sample function call with extended process list
-    result_df = post_request_sample(process_list=process_list, time_quanta=time_quanta)
+    # result_df = post_request_sample_roundrobin(process_list=process_list, time_quanta=time_quanta)
+
+    # result_df = post_request_sample_priority(process_list=process_list, is_preemptive=False)
     
     # Print the result
     print(result_df['attr'])
     print(result_df['gantt'])
 
-def post_request_sample(**kargs):
+def post_request_sample_roundrobin(**kargs):
     process_list = kargs.get("process_list", [])
     time_quanta = kargs.get("time_quanta", None)
     
@@ -36,6 +33,18 @@ def post_request_sample(**kargs):
         p_list.append(Process(process_id=process[0], arrival_time=process[1], burst_time=process[2]))
     
     scheduler = RoundRobinScheduler(p_list, time_quanta)
+    
+    return scheduler.get_dataframe()
+
+def post_request_sample_priority(**kargs):
+    process_list = kargs.get("process_list", [])
+    is_preemptive = kargs.get("is_preemptive", None)
+    
+    p_list = []
+    for process in process_list:
+        p_list.append(Process(process_id=process[0], arrival_time=process[1], burst_time=process[2], priority=process[3]))
+    
+    scheduler = PriorityScheduler(p_list, is_preemptive)
     
     return scheduler.get_dataframe()
 
--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 class Process:
-    def __init__(self, process_id ,arrival_time, burst_time):
+    def __init__(self, process_id ,arrival_time, burst_time, priority=None):
         self.process_id = process_id
         self.arrival_time = arrival_time
         self.burst_time = burst_time
         self.remaining_time = burst_time
+        self.priority = priority
         self.completion_time = None
         self.start_time = None
         self.turnaround_time = None
@@ -41,9 +42,8 @@ class RoundRobinScheduler:
         self.time = 0
     
     def _schedule(self):
-        process_count = len(self.process_list)
         
-        while len(self.terminated_queue) < process_count:
+        while len(self.terminated_queue) < len(self.process_list):
             for process in self.process_list:
                 if process.arrival_time <= self.time and process not in self.ready_queue and process not in self.waiting_queue and process not in self.terminated_queue:
                     self.ready_queue.append(process)
@@ -79,6 +79,103 @@ class RoundRobinScheduler:
     def get_dataframe(self):
         data = self._schedule()
         return create_process_dataframes(data)
+
+class PriorityScheduler:
+    def __init__(self, process_list: list[Process], is_preemptive: bool):
+        self.process_list = sorted(process_list, key=lambda process: process.arrival_time)
+        self.is_preemptive = is_preemptive
+        self.ready_queue = []
+        self.terminated_queue = []
+        self.waiting_queue = []
+        self.time = 0
+    
+    def _preemptive(self):
+        while len(self.terminated_queue) < len(self.process_list):
+            for process in self.process_list:
+                if process.arrival_time <= self.time and process not in self.ready_queue and process not in self.waiting_queue and process not in self.terminated_queue:
+                    self.ready_queue.append(process)
+            
+            self.ready_queue.extend(self.waiting_queue)
+            self.waiting_queue.clear()
+            self.ready_queue = sorted(self.ready_queue, key=lambda process: process.priority)
+
+            if self.ready_queue:
+                process = self.ready_queue.pop(0)
+                
+                if process.start_time == None:
+                    process.start_time = self.time
+                
+
+                if process.time_segments and process.time_segments[-1][1] == self.time:
+                    time_segment = [process.time_segments[-1][0], self.time + 1]
+                    process.time_segments[-1] = time_segment
+                else:
+                    time_segment = [self.time, self.time + 1]
+                    process.time_segments.append(time_segment)
+                
+                process.remaining_time -= 1
+                self.time += 1
+                
+                if process.remaining_time:
+                    self.waiting_queue.append(process)
+                else:
+                    process.completion_time = self.time
+                    process.turnaround_time = process.completion_time - process.arrival_time
+                    process.waiting_time = process.turnaround_time - process.burst_time
+                    process.response_time = process.start_time - process.arrival_time
+                    self.terminated_queue.append(process)
+            else:
+                self.time += 1
+                
+        return sorted(self.terminated_queue, key=lambda process: process.process_id)
+    
+    def _non_preemptive(self):
+        while len(self.terminated_queue) < len(self.process_list):
+            for process in self.process_list:
+                if process.arrival_time <= self.time and process not in self.ready_queue and process not in self.waiting_queue and process not in self.terminated_queue:
+                    self.ready_queue.append(process)
+            
+            self.ready_queue.extend(self.waiting_queue)
+            self.waiting_queue.clear()
+            self.ready_queue = sorted(self.ready_queue, key=lambda process: process.priority)
+
+            if self.ready_queue:
+                process = self.ready_queue.pop(0)
+                
+                if process.start_time == None:
+                    process.start_time = self.time
+                
+
+                time_segment = [self.time, self.time + process.remaining_time]
+                process.time_segments.append(time_segment)
+                
+                self.time += process.remaining_time
+                process.remaining_time = 0
+
+                
+                if process.remaining_time:
+                    self.waiting_queue.append(process)
+                else:
+                    process.completion_time = self.time
+                    process.turnaround_time = process.completion_time - process.arrival_time
+                    process.waiting_time = process.turnaround_time - process.burst_time
+                    process.response_time = process.start_time - process.arrival_time
+                    self.terminated_queue.append(process)
+            else:
+                self.time += 1
+                
+        return sorted(self.terminated_queue, key=lambda process: process.process_id)
+    
+    def get_dataframe(self):
+        if self.is_preemptive:
+            data = self._preemptive()
+        else:
+            data = self._non_preemptive()
+
+        return create_process_dataframes(data)
+
+
+        
         
 
 def create_process_dataframes(process_list):
@@ -100,6 +197,7 @@ def create_process_dataframes(process_list):
             'Process': p.process_id,
             'Arrival': p.arrival_time,
             'Burst': p.burst_time,
+            'Priority': p.priority,
             'Start': p.start_time,
             'Completion': p.completion_time,
             'Turnaround': p.turnaround_time,
